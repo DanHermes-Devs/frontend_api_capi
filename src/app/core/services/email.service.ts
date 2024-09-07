dced3bf97e34c3907fc0a1c2dc71f066899e4a56
Dan Hermes <80308086+DanHermes-Devs@users.noreply.github.com>
2024-09-06 21:29:15 -0600

    Se añade env al servicio email

--- src/app/core/services/email.service.ts
+++ src/app/core/services/email.service.ts
@@ -2,27 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Email } from '../../interfaces/email';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
+  apiurl = environment.api_url;
+
   constructor(private http: HttpClient) { }
 
   getEmailContactById(id: number): Observable<Email> {
-    return this.http.get<Email>(`http://api_rest_capi.test/api/email/${id}`);
+    return this.http.get<Email>(this.apiurl + "/email/" + id);
   }
 
   createEmailContact(email: Email): Observable<Email> {
-    return this.http.post<Email>(`http://api_rest_capi.test/api/email`, email);
+    return this.http.post<Email>(this.apiurl + "/email", email);
   }
 
   updateEmailContact(email: Email): Observable<Email> {
-    return this.http.put<Email>(`http://api_rest_capi.test/api/email/${email.id}`, email);
+    return this.http.put<Email>(this.apiurl + "/email/" + email.id, email);
   }
 
   deleteEmailContact(id: number): Observable<Email> {
-    return this.http.delete<Email>(`http://api_rest_capi.test/api/email/${id}`);
+    return this.http.delete<Email>(this.apiurl + "/email/" + id);
   }
 }
