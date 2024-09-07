2851cdb2cacfbe6caf93d93d57610aa3649d9d45
Dan Hermes <80308086+DanHermes-Devs@users.noreply.github.com>
2024-09-06 21:30:27 -0600

    Se añade env al servicio phone

--- src/app/core/services/phone.service.ts
+++ src/app/core/services/phone.service.ts
@@ -2,27 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Phone } from '../../interfaces/phone';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhoneService {
 
+  apiurl = environment.api_url;
+
   constructor(private http: HttpClient) { }
 
   getPhoneContactById(id: number): Observable<Phone> {
-    return this.http.get<Phone>(`http://api_rest_capi.test/api/phone/${id}`);
+    return this.http.get<Phone>(this.apiurl + "/phone/" + id);
   }
 
   createPhoneContact(phone: Phone): Observable<Phone> {
-    return this.http.post<Phone>(`http://api_rest_capi.test/api/phone`, phone);
+    return this.http.post<Phone>(this.apiurl + "/phone", phone);
   }
 
   updatePhoneContact(phone: Phone): Observable<Phone> {
-    return this.http.put<Phone>(`http://api_rest_capi.test/api/phone/${phone.id}`, phone);
+    return this.http.put<Phone>(this.apiurl + "/phone/" + phone.id, phone);
   }
 
   deletePhoneContact(id: number): Observable<Phone> {
-    return this.http.delete<Phone>(`http://api_rest_capi.test/api/phone/${id}`);
+    return this.http.delete<Phone>(this.apiurl + "/phone/" + id);
   }
 }
