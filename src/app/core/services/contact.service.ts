81f6fb7c71dd2c9aa3596bf4f5034a97d8530c8a
Dan Hermes <80308086+DanHermes-Devs@users.noreply.github.com>
2024-09-06 21:28:21 -0600

    Se añade el env al servicio contacto

--- src/app/core/services/contact.service.ts
+++ src/app/core/services/contact.service.ts
@@ -2,35 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Contact } from '../../interfaces/contact';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
+  apiurl = environment.api_url;
+
   constructor(private http: HttpClient) { }
 
   getContactList(): Observable<Contact> {
-    return this.http.get<Contact>(`http://api_rest_capi.test/api/contacts`);
+    return this.http.get<Contact>(this.apiurl + "/contacts");
   }
 
   getContactById(id: number): Observable<Contact> {
-    return this.http.get<Contact>(`http://api_rest_capi.test/api/contact/${id}`);
+    return this.http.get<Contact>(this.apiurl + "/contact/" + id);
   }
 
   getContactBySearch(search: string): Observable<Contact> {
-    return this.http.get<Contact>(`http://api_rest_capi.test/api/contact/filter?term=${search}&per_page=10`);
+    return this.http.get<Contact>(this.apiurl + "/contact/filter?term=" + search + "&per_page=10");
   }
 
   createContact(contact: Contact): Observable<Contact> {
-    return this.http.post<Contact>(`http://api_rest_capi.test/api/contacts`, contact);
+    return this.http.post<Contact>(this.apiurl + "/contacts", contact);
   }
 
   updateContact(contact: Contact): Observable<Contact> {
-    return this.http.put<Contact>(`http://api_rest_capi.test/api/contact/${contact.id}`, contact);
+    return this.http.put<Contact>(this.apiurl + "/contact/" + contact.id, contact);
   }
 
   deleteContact(id: number): Observable<Contact> {
-    return this.http.delete<Contact>(`http://api_rest_capi.test/api/contact/${id}`);
+    return this.http.delete<Contact>(this.apiurl + "/contact/" + id);
   }
 }
