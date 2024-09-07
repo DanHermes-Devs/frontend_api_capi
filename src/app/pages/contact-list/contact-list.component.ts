9aa45a156f2237457d0a1472dc57f5cdc6357deb
Dan Hermes <80308086+DanHermes-Devs@users.noreply.github.com>
2024-09-07 10:29:50 -0600

    modificamos el componente contact list, para el buscador

--- src/app/pages/contact-list/contact-list.component.ts
+++ src/app/pages/contact-list/contact-list.component.ts
@@ -1,27 +1,50 @@
-import { Component, Input } from '@angular/core';
-import { Contact } from '../../interfaces/contact';
-import { ContactService } from '../../core/services/contact.service';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
 import { CommonModule } from '@angular/common';
+import { ContactService } from '../../core/services/contact.service';
+import { Observable, Subject } from 'rxjs';
 import { PaginatedContacts } from '../../interfaces/PaginatedContacts';
+import { debounceTime } from 'rxjs/operators';
+import { Contact } from '../../interfaces/contact';
+import { Address } from '../../interfaces/address';
+import { Email } from '../../interfaces/email';
+import { Phone } from '../../interfaces/phone';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
 })
-
 export class ContactListComponent {
   public contactResults$!: Observable<PaginatedContacts>;
+  isLoading: boolean = false;
 
   constructor(private service: ContactService) {}
 
   ngOnInit(): void {
-    this.contactResults$ = this.service.getContactList()
+    this.contactResults$ = this.service.getContactList();
+  }
+
+  search(term: string): void {
+    if (term.trim() === '') {
+      this.contactResults$ = this.service.getContactList();
+    } else {
+      this.isLoading = true;
+      this.contactResults$ = this.service.getContactBySearch(term);
+      this.isLoading = false;
+    }
+  }
+
+  onInputChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement | null;
+    if (inputElement) {
+      this.search(inputElement.value);
+    }
   }
 
   loadPage(url: string): void {
